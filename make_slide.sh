ebcf04b253642a5373d36993b8d9d1bec31a4cbb
yung-web <81741647+yung-web@users.noreply.github.com>
2021-04-02 21:16:49 +0900

    ..

---
+++ make_slide.sh
@@ -0,0 +1,13 @@
+echo "\def\pdfmode{slidemode}" > mode.tex
+
+(cd 0.Introduction; pdflatex -jobname=0.Intro-slide main.tex)		
+(cd 1.Probabilistic_Model; pdflatex -jobname=1.PM-slide main.tex)		
+(cd 2.Conditioning_Independence; pdflatex -jobname=2.CPI-slide main.tex)				
+(cd 3.RandomVariable-Part1; pdflatex -jobname=3.RV-PartOne-slide main.tex)			
+(cd 4.RandomVariable-Part2; pdflatex -jobname=4.RV-PartTwo-slide main.tex)			
+(cd 5.RandomVariable-Part3; pdflatex -jobname=5.RV-PartThree-slide main.tex;pdflatex -jobname=5.RV-PartThree-slide main.tex)				
+(cd 6.Statistical_Inference; pdflatex -jobname=6.SI-slide main.tex;pdflatex -jobname=6.SI-slide main.tex)				
+(cd 7.Limit_Theorems; pdflatex -jobname=7.Limit-slide main.tex)				
+(cd 8.RandomProcess-Part1; pdflatex -jobname=8.RP-PartOne-slide main.tex)
+(cd 9.RandomProcess-Part2; pdflatex -jobname=9.RP-PartTwo-slide main.tex)		
+
---
+++ print.sh
@@ -0,0 +1,48 @@
+(cd 01.Introduction;
+ pdfjam --nup 1x2 1.intro.pdf --outfile 1.intro-2.pdf;
+ pdfjam --nup 2x2 1.intro.pdf --outfile 1.intro-4.pdf --landscape)
+
+(cd 02.LinearAlgebra;
+ pdfjam --nup 1x2 2.LA.pdf --outfile 2.LA-2.pdf;
+ pdfjam --nup 2x2 2.LA.pdf --outfile 2.LA-4.pdf --landscape)
+
+(cd 03.Geometry;
+ pdfjam --nup 1x2 3.AG.pdf --outfile 3.AG-2.pdf;
+ pdfjam --nup 2x2 3.AG.pdf --outfile 3.AG-4.pdf --landscape)
+
+(cd 04.MatrixDecomposition;
+ pdfjam --nup 1x2 4.MD.pdf --outfile 4.MD-2.pdf;
+ pdfjam --nup 2x2 4.MD.pdf --outfile 4.MD-4.pdf --landscape)
+
+(cd 05.VectorCaculus;
+ pdfjam --nup 1x2 5.VC.pdf --outfile 5.VC-2.pdf;
+ pdfjam --nup 2x2 5.VC.pdf --outfile 5.VC-4.pdf --landscape)
+
+(cd 06.Probability;
+ pdfjam --nup 1x2 6.PD.pdf --outfile 6.PD-2.pdf;
+ pdfjam --nup 2x2 6.PD.pdf --outfile 6.PD-4.pdf --landscape)
+
+(cd 07.Optimization;
+ pdfjam --nup 1x2 7.OPT.pdf --outfile 7.OPT-2.pdf;
+ pdfjam --nup 2x2 7.OPT.pdf --outfile 7.OPT-4.pdf --landscape)
+
+(cd 08.Model_Data;
+ pdfjam --nup 1x2 8.MMD.pdf --outfile 8.MMD-2.pdf;
+ pdfjam --nup 2x2 8.MMD.pdf --outfile 8.MMD-4.pdf --landscape)
+
+(cd 09.LinearRegression;
+ pdfjam --nup 1x2 9.LR.pdf --outfile 9.LR-2.pdf;
+ pdfjam --nup 2x2 9.LR.pdf --outfile 9.LR-4.pdf --landscape)
+
+(cd 10.PCA;
+ pdfjam --nup 1x2 10.PCA.pdf --outfile 10.PCA-2.pdf;
+ pdfjam --nup 2x2 10.PCA.pdf --outfile 10.PCA-4.pdf --landscape)
+
+(cd 11.DensityEstimation;
+ pdfjam --nup 1x2 11.GMM.pdf --outfile 11.GMM-2.pdf;
+ pdfjam --nup 2x2 11.GMM.pdf --outfile 11.GMM-4.pdf --landscape)
+
+(cd 12.SVM;
+ pdfjam --nup 1x2 12.SVM.pdf --outfile 12.SVM-2.pdf;
+ pdfjam --nup 2x2 12.SVM.pdf --outfile 12.SVM-4.pdf --landscape)
+
